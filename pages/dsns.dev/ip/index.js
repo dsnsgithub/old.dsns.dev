b826f3c9299a4879a03dc103a3b4b4cf69897083
DSNS <dominicseung2@gmail.com>
2022-06-03 10:41:26 -0700

    added screen resolution to ip address finder

--- pages/dsns.dev/ip/index.js
+++ pages/dsns.dev/ip/index.js
@@ -2,11 +2,16 @@
 async function run() {
 	const ipDIV = document.getElementById("ip");
 	const userAgentDIV = document.getElementById("useragent");
+	const screenResolutionDiv = document.getElementById("screenresolution");
+
 	const headers = await fetch("/ipAPI").then((response) => response.json());
 
 	ipDIV.innerHTML = headers["x-forwarded-for"];
+
 	const cleanUserAgent = headers["user-agent"].replace(/</g, "&lt;").replace(/>/g, "&gt;");
 	userAgentDIV.innerHTML = cleanUserAgent;
+
+	screenResolutionDiv.innerHTML = `${screen.width}x${screen.height}`;
 }
 
 run();
