aa92e9c26812c239b5831245ba7cb6ac7964f01a
dsnsgithub <dominicseung2@gmail.com>
2022-07-04 22:42:59 -0700

    add many audio filetypes to yt downloader

--- pages/dsns.dev/mp3/index.js
+++ pages/dsns.dev/mp3/index.js
@@ -1,3 +1,41 @@
+const { createFFmpeg } = FFmpeg;
+
+async function convertFileFormat(format, type, youtubeID) {
+	// Use ffmpeg.wasm to convert the downloaded webm file to an mp3 file
+
+	const sourceBuffer = await fetch(`/api/youtube/${youtubeID}`).then((r) => r.arrayBuffer());
+
+	// create the FFmpeg instance and load it
+	const ffmpeg = createFFmpeg({ log: true });
+	await ffmpeg.load();
+
+	// create a progress bar that displays the progress of the conversion
+	ffmpeg.setProgress(({ ratio }) => {
+		document.getElementById("progressBar").value = ratio;
+	});
+
+	// write the WEBM to the FFmpeg file system
+	ffmpeg.FS("writeFile", `${youtubeID}.webm`, new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength));
+
+	// run the FFmpeg command-line tool, converting the WEBM into an MP3
+	await ffmpeg.run("-i", `${youtubeID}.webm`, `${youtubeID}.${format}`);
+
+	// read the MP3 file back from the FFmpeg file system
+	const output = ffmpeg.FS("readFile", `${youtubeID}.${format}`);
+
+	// download the MP3 file
+	const blob = new Blob([output], { type: type });
+	const url = URL.createObjectURL(blob);
+	const a = document.createElement("a");
+	a.href = url;
+	a.download = `${youtubeID}.${format}`;
+	a.click();
+	URL.revokeObjectURL(url);
+
+	ffmpeg.FS("unlink", `${youtubeID}.webm`);
+	ffmpeg.FS("unlink", `${youtubeID}.${format}`);
+}
+
 async function downloadMP3() {
 	try {
 		const downloadLink = document.getElementById("downloadLink").value;
@@ -15,45 +53,20 @@ async function downloadMP3() {
 		}
 
 		const fileType = document.getElementById("fileType").value;
+
 		if (fileType == "mp3") {
-			// Use ffmpeg.wasm to convert the downloaded webm file to an mp3 file
-			const { createFFmpeg } = FFmpeg;
-
-			const sourceBuffer = await fetch(`/api/youtube/${youtubeID}`).then((r) => r.arrayBuffer());
-
-			// create the FFmpeg instance and load it
-			const ffmpeg = createFFmpeg({ log: true });
-			await ffmpeg.load();
-			
-
-			// create a progress bar that displays the progress of the conversion
-			ffmpeg.setProgress(({ ratio }) => {
-				document.getElementById("progressBar").value = ratio;
-			});
-
-			// write the WEBM to the FFmpeg file system
-			ffmpeg.FS("writeFile", `${youtubeID}.webm`, new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength));
-
-			// run the FFmpeg command-line tool, converting the WEBM into an MP3
-			await ffmpeg.run("-i", `${youtubeID}.webm`, `${youtubeID}.mp3`);
-
-			// read the MP3 file back from the FFmpeg file system
-			const output = ffmpeg.FS("readFile", `${youtubeID}.mp3`);
-
-			// download the MP3 file
-			const blob = new Blob([output], { type: "audio/mpeg" });
-			const url = URL.createObjectURL(blob);
-			const a = document.createElement("a");
-			a.href = url;
-			a.download = `${youtubeID}.mp3`;
-			a.click();
-			URL.revokeObjectURL(url);
-		} 
-		else if (fileType == "webm") {
+			await convertFileFormat("mp3", "audio/mpeg", youtubeID);
+		} else if (fileType == "wav") {
+			await convertFileFormat("wav", "audio/wav", youtubeID);
+		} else if (fileType == "flac") {
+			await convertFileFormat("flac", "audio/flac", youtubeID);
+		} else if (fileType == "aac") {
+			await convertFileFormat("aac", "audio/aac", youtubeID);
+		} else if (fileType == "ogg") {
+			await convertFileFormat("ogg", "audio/ogg", youtubeID);
+		} else if (fileType == "webm") {
 			window.open(`/api/youtube/${youtubeID}`, "_blank");
 		}
-
-
 	} catch (e) {
 		console.log(e);
 	}
