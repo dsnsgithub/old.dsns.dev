67c6ea9c145df6464bc245c9b3797f30fb0170de
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2022-10-27 22:52:39 -0700

    fix bugs in onlyeggrolls.com

--- pages/onlyeggrolls.com/menu/index.js
+++ pages/onlyeggrolls.com/menu/index.js
@@ -1,38 +1,41 @@
+const totalCostElem = document.getElementById("totalCost");
+const shoppingCartElem = document.getElementById("cart");
+
 let shoppingCartList = [];
 let costList = [];
 
 let totalCost = 0;
-
-const totalCostElem = document.getElementById("totalCost");
-const shoppingCartElem = document.getElementById("cart");
 
 function addToShoppingCart(item, cost) {
 	shoppingCartList.push(item);
 	costList.push(cost);
 
 	totalCost += cost;
-
 	totalCostElem.textContent = `Total Cost: $${totalCost}`;
 
 	const newItem = document.createElement("li");
 	newItem.innerHTML = item;
 
 	shoppingCartElem.appendChild(newItem);
-
 }
 
 function removeItem() {
+	if (totalCost <= 0) return;
+
 	const removedCost = costList[costList.length - 1];
 	costList.pop();
 
 	totalCost = totalCost - removedCost;
-
 	shoppingCartElem.removeChild(shoppingCartElem.lastElementChild);
 	totalCostElem.innerHTML = `Total Cost: $${totalCost}`;
 }
 
-
 function completeTransaction() {
+	if (totalCost <= 0) {
+		alert("Please buy at least one item before finalizing the purchase.");
+		return;
+	}
+
 	alert(`Thanks for shopping at OnlyEggrolls!\nThe total cost of this purchase is $${totalCost}. `);
 	shoppingCartElem.innerHTML = "";
 	totalCost = 0;
