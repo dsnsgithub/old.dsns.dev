a01451f856ac133334f57a6808077802faae9bbc
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2022-10-27 18:42:38 -0700

    refactor /mp3 and /learn

--- pages/dsns.dev/learn/index.js
+++ pages/dsns.dev/learn/index.js
@@ -1,205 +1,73 @@
-const raw = `師母/师母 - An honorific term used when addressing the teacher's wife
-shī mǔ
-些 - some
-xiē
-維生素/维生素 - vitamin
-wéi shēng sù
-永遠/永远 - forever
-yǒng yuǎn
-健康 - healthy
-jiàn kāng
-年輕/年轻 - young
-nián qīng
-破費/破费 - you spend too much money, wen to great expense
-pò fèi
-小意思 - no big deal
-xiǎo yì si
-難得/难得 - seldom, rarely
-nán dé
-特地 - specially
-tè dì
-拿手菜 - best cooking
-ná shǒu cài
-涼/凉 - cool, cold
-liáng
-精神 - energetic, high-spirited
-jīng shen
-白頭髮/白头发 - gray or white hair
-bái tóu fa
-眼睛 - eye(s)
-yǎn jīng
-花 - blurred eyesight, flower
-huā
-適應/适应 - to get used to
-shì yìng
-時差/时差 - time difference, jetlag
-shí chā
-白天 - daytime
-bái tiān
-睏 - sleepy, dozy
-kùn
-睡不著/睡不着 - unable to sleep
-shuì bu zháo
-街道 - street
-jiē dào
-習慣/习惯 - to be used to
-xí guàn
-長/长 - to grow
-cháng
-年糕 - New Year cake, rice cake
-nián gāo
-味道 - taste
-wèi dao
-地道 - original, genuine, authentic
-dì dao
-香 - aroma, aromatic
-xiāng
-俱全 - all included,, altogether
-jù quán
-飽/饱 - full
-bǎo
-只是 - just, simply
-zhǐ shì
-家常便飯/家常便饭 - homemade meal
-jiā cháng biàn fàn
-相聚 - to be together, get together
-xiāng jù
-乾杯/干杯 - to toast, cheers
-gān bēi
-有朋自遠方來，不亦樂乎?/有朋自远方来，不亦乐乎？ - Isn't it pleasant to have friends coming from afar?
-yǒu péng zì yuǎn fāng lái , bú yì yùe hū ?`.split("\n");
+async function parseData() {
+	const charRes = await fetch("characters.txt").then((res) => res.text());
+	const sentenceRes = await fetch("sentencepatterns.txt").then((res) => res.text());
 
-//! Parse Characters ------------------------------------------------------
-let characters = [];
-for (let i = 0; i < raw.length; i = i + 2) {
-	let character = raw[i];
-	let [char, def] = character.split(" - ");
-	if (char.indexOf("/") > -1) {
-		let [traditional, simple] = char.split("/");
-		characters.push([simple, def]);
-	} else {
-		characters.push([char, def]);
-	}
-}
+	const charactersRaw = charRes.split("\r\n");
+	const sentenceRaw = sentenceRes.split("\r\n");
 
-//! Parse Pinyin ------------------------------------------------------
-const pinyinArray = [];
-let count = 0;
-for (let i = 1; i < raw.length; i = i + 2) {
-	const pinyin = raw[i].split(" ");
-
-	let completePinyin = "";
-	for (const char of pinyin) {
-		const tone1 = /[āēīōūǖ]/;
-		const tone2 = /[áéíóúǘ]/;
-		const tone3 = /[ǎěǐǒǔǚ]/;
-		const tone4 = /[àèìòùǜ]/;
-
-		let newCharacter = char
-			.replace(/[āáǎà]/g, "a")
-			.replace(/[ēéěè]/g, "e")
-			.replace(/[īíǐì]/g, "i")
-			.replace(/[ōóǒò]/g, "o")
-			.replace(/[ūúǔù]/g, "u")
-			.replace(/[ǖǘǚǜü]/g, "v");
-
-		if (tone1.test(char)) newCharacter += "1";
-		if (tone2.test(char)) newCharacter += "2";
-		if (tone3.test(char)) newCharacter += "3";
-		if (tone4.test(char)) newCharacter += "4";
-
-		completePinyin += newCharacter;
+	// Parse Characters
+	const characters = [];
+	for (let i = 0; i < charactersRaw.length; i = i + 2) {
+		let character = charactersRaw[i];
+		let [char, def] = character.split(" - ");
+		if (char.indexOf("/") > -1) {
+			let [traditional, simple] = char.split("/");
+			characters.push([simple, def]);
+		} else {
+			characters.push([char, def]);
+		}
 	}
 
-	pinyinArray.push([completePinyin, characters[count][0]]);
-	count += 1;
-}
+	// Parse Sentence Patterns
+	const sentencePatterns = [];
+	for (const line of sentenceRaw) {
+		if (line.indexOf("-") == -1) continue;
 
-//! Sentence Patterns ----------------------------------------------------------------------------------
+		let [sentence, answer] = line.split(" - ");
 
-const sentenceRaw = `老師的太太是 ______。
-老师的太太是 ______。 - 師母/师母
-這 ______ 東西都不是我的。
-这 ______ 东西都不是我的。 - 些
-媽媽每天早上都吃很多 ______，讓自己看來年輕一點。
-妈妈每天早上都吃很多 ______，让自己看来年轻一点。 - 維生素/维生素
-媽媽說: "我 ______ 都愛你。"
-妈妈说: "我 ______ 都爱你。" - 永遠/永远
-聽說她常生病，好像不太 ______。
-听说她常生病，好像不太 ______。 - 健康
-他已經五十歲了，可是看來很 ______。
-他已经五十岁了，可是看来很 ______。 - 年輕/轻
-你怎麼帶禮物來看我，讓你 ______ 了!
-你怎么带礼物来看我，让你 ______ 了! - 破費/破费
-"這件衣服很貴吧!" "沒關係，______。"
-"这件衣服很贵吧!" "没关系，______。" - 小意思
-這個星期 ______ 沒有功課，我們去看電影吧!
-这个星期 ______ 沒有功课，我们去看电影吧! - 難得/难得
-我男朋友知道我喜歡吃炒麵，他 ______ 去那家小吃店買給我吃。
-我男朋友知道我喜欢吃炒面，他 ______ 去那家小吃店买给我吃。 - 特地
-"請你嚐嚐我的 ______。" "真好吃! 你怎麼做的?"
-"请你尝尝我的 ______。" "真好吃! 你怎么做的?" - 拿手菜
-這杯茶已經 ______ 了，你要一杯熱的嗎?
-这杯茶已经 ______ 了，你要一杯热的吗? - 涼/凉
-他昨天晚上睡了很久，所以現在看來很 ______。
-他昨天晚上睡了很久，所以現在看来很 ______。 - 精神
-"老師三十歲的時候已經開始有 ______ 了。" "真的嗎?"
-"老师三十岁的时候已经开始有 ______ 了。" "真的吗?" - 白頭髮/白头发
-她那雙大 ______ 真好看! 好像會說話。
-她那双大 ______ 真好看! 好像会说话。 - 眼睛
-爸爸老了，眼睛 ______ 了，走路也慢了。 - 花
-我還不 ______ 這裡的天氣，太熱了。
-我还不 ______ 这里的天气，太热了。 - 適應/适应
-我很不喜歡去國外旅行，因為適應 ______ 讓我很不舒服。
-我很不喜欢去国外旅行，因为适应 ______ 让我很不舒服。 - 時差/时差
-"我喜歡 ______ 睡覺，晚上找東西吃。""你是貓嗎?"
-"我喜欢 ______ 睡觉，晚上找东西吃。""你是猫吗?" - 白天
-我昨天沒睡覺，現在覺得很 ______。
-我昨天沒睡觉，現在觉得很 ______。 - 睏
-我很累，可是 ______。 - 睡不著/睡不着
-中國新年的時候，______ 上到處都是買東西的人。
-中国新年的时候，______ 上到处都是买东西的人。 - 街道
-我 ______ 先吃飯，再做功課。我不喜歡先做功課再吃飯。
-我 ______ 先吃饭，再做功课。我不喜欢先做功课再吃饭。 - 習慣/习惯
-他常鍛煉，所以 ______ 得很高。
-他常锻炼，所以 ______ 得很高。 - 長
-過新年的時候，中國人要吃 ______。
-过新年的时候，中国人要吃 ______。 - 年糕
-媽媽包的餃子 ______ 好極了!
-妈妈包的饺子 ______ 好极了! - 味道
-我媽媽做的炒年糕味道很 ______。
-我妈妈做的炒年糕味道很 ______。 - 地道
-"這些花真 ______!" "你喜歡嗎? 送給你吧!"
-"这些花真 ______!" "你喜欢吗? 送给你吧!" - 香
-她的拿手菜很好吃也很好看，真是色香味 ______。 - 俱全
-我吃了兩碗飯，現在覺得很 _____。
-我吃了两碗饭，现在觉得很 _____。 - 飽/饱
-"這盤炒麵真好吃!" "哪裡，______ 家常便飯。快吃吧!"
-"这盘炒面真好吃!" "哪里，______ 家常便饭。快吃吧!" - 只是
-晚上來我家吃飯吧! 都只是一些 ______。
-晚上来我家吃饭吧! 都只是一些 ______。 - 家常便飯/饭
-你很久沒有和男朋友 ______ 了，一起出去玩吧! - 相聚
-好久不見，咱們來 ______ 吧!
-好久不见，咱们来 ______ 吧! - 乾杯/干杯
-"我的老朋友要來看我了，真高興!" "是啊! _______。"
-"我的老朋友要来看我了，真高兴!" "是啊! _______。" - 有朋自遠方來，不亦樂乎?/有朋自远方来，不亦乐乎?`.split("\n");
-
-let sentencePatterns = [];
-for (const line of sentenceRaw) {
-	if (line.indexOf("-") == -1) continue;
-
-	let [sentence, answer] = line.split(" - ");
-
-	if (answer.indexOf("/") > -1) {
-		let [traditional, simplified] = answer.split("/");
-		sentencePatterns.push([simplified, sentence]);
-	} else {
-		sentencePatterns.push([answer, sentence]);
+		if (answer.indexOf("/") > -1) {
+			let [traditional, simplified] = answer.split("/");
+			sentencePatterns.push([simplified, sentence]);
+		} else {
+			sentencePatterns.push([answer, sentence]);
+		}
 	}
-}
 
-// ! MAIN ----------------------------------------------------------------------------------------------
-const showCharacter = document.getElementById("showCharacter");
+	// Parse Pinyin
+	const pinyinArray = [];
+	let count = 0;
+	for (let i = 1; i < charactersRaw.length; i = i + 2) {
+		const pinyin = charactersRaw[i].split(" ");
+
+		let completePinyin = "";
+		for (const char of pinyin) {
+			const tone1 = /[āēīōūǖ]/;
+			const tone2 = /[áéíóúǘ]/;
+			const tone3 = /[ǎěǐǒǔǚ]/;
+			const tone4 = /[àèìòùǜ]/;
+
+			let newCharacter = char
+				.replace(/[āáǎà]/g, "a")
+				.replace(/[ēéěè]/g, "e")
+				.replace(/[īíǐì]/g, "i")
+				.replace(/[ōóǒò]/g, "o")
+				.replace(/[ūúǔù]/g, "u")
+				.replace(/[ǖǘǚǜü]/g, "v");
+
+			if (tone1.test(char)) newCharacter += "1";
+			if (tone2.test(char)) newCharacter += "2";
+			if (tone3.test(char)) newCharacter += "3";
+			if (tone4.test(char)) newCharacter += "4";
+
+			completePinyin += newCharacter;
+		}
+
+		pinyinArray.push([completePinyin, characters[count][0]]);
+		count += 1;
+	}
+
+	return [characters, sentencePatterns, pinyinArray];
+}
 
 function showNewDefinition() {
 	if (data.length == 0) {
@@ -207,13 +75,11 @@ function showNewDefinition() {
 		return;
 	}
 	const randomIndex = Math.floor(Math.random() * data.length);
-	showCharacter.innerHTML = `Type the ${type} for <b>${data[randomIndex][1]}</b>`;
+	characterElem.innerHTML = `Type the ${type} for <b>${data[randomIndex][1]}</b>`;
 	return randomIndex;
 }
 
-let wrong = false;
-
-function run() {
+function checkAnswer() {
 	const input = document.getElementById("chinese");
 
 	if (input.value == data[index][0]) {
@@ -244,27 +110,7 @@ function run() {
 	}
 }
 
-const submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", run);
-
-document.onkeyup = function (event) {
-	if (event.key == "Enter") {
-		event.preventDefault();
-		run();
-	}
-};
-
-//! CONFIG ----------------------------------------------------------------------------------------------
-
-let data = [...characters];
-let type = "chinese character";
-
-//? Initiate
-let index = showNewDefinition();
-
-const selectElem = document.getElementById("select");
-
-selectElem.addEventListener("change", function () {
+function selectOption() {
 	if (selectElem.value == "Pinyin") {
 		data = [...pinyinArray];
 		type = "pinyin";
@@ -281,4 +127,32 @@ selectElem.addEventListener("change", function () {
 
 		index = showNewDefinition();
 	}
-});
+}
+
+const characterElem = document.getElementById("showCharacter");
+const selectElem = document.getElementById("select");
+const submitButton = document.getElementById("submit");
+
+let wrong = false;
+let data = [];
+let type = "chinese character";
+let index = 0;
+let characters, sentencePatterns, pinyinArray;
+
+async function run() {
+	[characters, sentencePatterns, pinyinArray] = await parseData();
+	data = [...characters];
+
+	index = showNewDefinition();
+	selectElem.addEventListener("change", selectOption);
+	submitButton.addEventListener("click", checkAnswer);
+
+	document.onkeyup = function (event) {
+		if (event.key == "Enter") {
+			event.preventDefault();
+			checkAnswer();
+		}
+	};
+}
+
+run();
--- pages/dsns.dev/mp3/index.js
+++ pages/dsns.dev/mp3/index.js
@@ -1,22 +1,22 @@
 const { createFFmpeg } = FFmpeg;
+
 const downloadStatus = document.getElementById("downloadStatus");
+const conversionStatus = document.getElementById("conversionStatus");
 
 const round = (number, decimalPlaces) => {
 	const factorOfTen = Math.pow(10, decimalPlaces);
 	return Math.floor(number * factorOfTen) / factorOfTen;
 };
 
-async function convertFileFormat(format, type, youtubeID) {
-	// Use ffmpeg.wasm to convert the downloaded webm file to an mp3 file
+// Downloads file with progress bar, splits into chunks
+async function downloadAPI(url) {
 	let timer = 0;
 	const clock = setInterval(() => {
 		timer += 1;
 		downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
 	}, 10);
 
-	downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
-
-	const response = await fetch(`/api/youtube/${youtubeID}`);
+	const response = await fetch(url);
 	clearInterval(clock);
 
 	if (response.status != 200) {
@@ -25,7 +25,6 @@ async function convertFileFormat(format, type, youtubeID) {
 	}
 
 	const reader = response.body.getReader();
-
 	let receivedLength = 0;
 	let chunks = [];
 	while (true) {
@@ -34,50 +33,51 @@ async function convertFileFormat(format, type, youtubeID) {
 
 		chunks.push(value);
 		receivedLength += value.length;
-
 		downloadStatus.innerText = "Downloaded " + round(receivedLength / 1000000, 2) + "MB";
 	}
 
-	// Step 4: concatenate chunks into single Uint8Array
-	let chunksAll = new Uint8Array(receivedLength);
+	downloadStatus.innerHTML = "Download Complete";
+	return [chunks, receivedLength];
+}
+
+async function convertChunkToArray(chunks, receivedLength) {
+	let chunksAll = new Uint8Array(receivedLength); // (4.1)
 	let position = 0;
 	for (let chunk of chunks) {
-		chunksAll.set(chunk, position);
+		chunksAll.set(chunk, position); // (4.2)
 		position += chunk.length;
 	}
 
-	downloadStatus.innerHTML = "Download Complete";
+	return chunksAll;
+}
 
-	// create the FFmpeg instance and load it
-	const ffmpeg = createFFmpeg({ log: true });
-	await ffmpeg.load();
-
-	// create a progress bar that displays the progress of the conversion
-	ffmpeg.setProgress(({ ratio }) => {
-		const conversionStatus = document.getElementById("conversionStatus");
-		const percent = round(ratio * 100, 2);
-
-		conversionStatus.innerHTML = percent + "%";
-	});
-
-	// write the WEBM to the FFmpeg file system
-	// ffmpeg.FS("writeFile", `audio.webm`, new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength));
-	ffmpeg.FS("writeFile", `audio.webm`, chunksAll);
-
-	// run the FFmpeg command-line tool, converting the WEBM into an MP3
-	await ffmpeg.run("-i", `audio.webm`, `audio.${format}`);
-
-	// read the MP3 file back from the FFmpeg file system
-	const output = ffmpeg.FS("readFile", `audio.${format}`);
-
-	// download the MP3 file
-	const blob = new Blob([output], { type: type });
+async function downloadFile(blob, fileName) {
 	const url = URL.createObjectURL(blob);
 	const a = document.createElement("a");
 	a.href = url;
-	a.download = `${youtubeID}.${format}`;
+	a.download = fileName;
 	a.click();
 	URL.revokeObjectURL(url);
+}
+
+async function convertFileFormat(format, type, youtubeID) {
+	const [chunks, receivedLength] = await downloadAPI(`/api/youtube/${youtubeID}`);
+	const chunksAll = await convertChunkToArray(chunks, receivedLength);
+
+	const ffmpeg = createFFmpeg({ log: true });
+	await ffmpeg.load();
+
+	ffmpeg.setProgress(({ ratio }) => {
+		const percent = round(ratio * 100, 2);
+		conversionStatus.innerHTML = percent + "%";
+	});
+
+	ffmpeg.FS("writeFile", `audio.webm`, chunksAll);
+	await ffmpeg.run("-i", `audio.webm`, `audio.${format}`);
+	const output = ffmpeg.FS("readFile", `audio.${format}`);
+
+	const blob = new Blob([output], { type: type });
+	await downloadFile(blob, `${youtubeID}.${format}`);
 
 	ffmpeg.FS("unlink", `audio.webm`);
 	ffmpeg.FS("unlink", `audio.${format}`);
@@ -105,67 +105,20 @@ async function downloadMP3() {
 
 		const fileType = document.getElementById("fileType").value;
 
-		if (fileType == "mp3") {
-			await convertFileFormat("mp3", "audio/mpeg", youtubeID);
-		} else if (fileType == "wav") {
-			await convertFileFormat("wav", "audio/wav", youtubeID);
-		} else if (fileType == "flac") {
-			await convertFileFormat("flac", "audio/flac", youtubeID);
-		} else if (fileType == "aac") {
-			await convertFileFormat("aac", "audio/aac", youtubeID);
-		} else if (fileType == "ogg") {
-			await convertFileFormat("ogg", "audio/ogg", youtubeID);
+		if (fileType.includes("mp4")) {
+			const [chunks, receivedLength] = await downloadAPI(`https://dsnsdev.dsnsrepl.repl.co/${youtubeID}`);
+			const chunksAll = await convertChunkToArray(chunks, receivedLength);
+			const result = JSON.parse(new TextDecoder("utf-8").decode(chunksAll));
+
+			if (fileType == "mp4audio") window.open(result["highest"]["url"], "_blank");
+			if (fileType == "mp4high") window.open(result["highestvideo"]["url"], "_blank");
 		} else if (fileType == "webm") {
-			let timer = 0;
-			const clock = setInterval(() => {
-				timer += 1;
-				downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
-			}, 10);
+			const res = await downloadAPI(`/api/youtube/${youtubeID}`);
+			const blob = new Blob(res[0]);
 
-			downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
-			const response = await fetch(`/api/youtube/${youtubeID}`);
-			if (response.status != 200) alert(await response.text());
-			clearInterval(clock);
-
-			const reader = response.body.getReader();
-
-			let receivedLength = 0;
-			let chunks = [];
-			while (true) {
-				const { done, value } = await reader.read();
-				if (done) break;
-
-				chunks.push(value);
-				receivedLength += value.length;
-
-				downloadStatus.innerText = "Downloaded " + round(receivedLength / 1000000, 2) + "MB";
-			}
-
-			const blob = new Blob(chunks);
-			const url = window.URL.createObjectURL(blob);
-			const a = document.createElement("a");
-			a.href = url;
-			a.download = `${youtubeID}.webm`;
-			a.click();
-			window.URL.revokeObjectURL(url);
+			await downloadFile(blob, `${youtubeID}.webm`);
 		} else {
-			let timer = 0;
-			const clock = setInterval(() => {
-				timer += 1;
-				downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
-			}, 10);
-
-			downloadStatus.innerHTML = "Downloading..." + ` (${timer / 100} seconds)`;
-			const req = await fetch(`https://dsnsdev.dsnsrepl.repl.co/${youtubeID}`);
-			clearInterval(clock);
-
-			if (req.status != 200) {
-				alert(await req.text());
-			} else {
-				const response = await req.json();
-				if (fileType == "mp4audio") window.open(response["highest"]["url"], "_blank");
-				if (fileType == "mp4high") window.open(response["highestvideo"]["url"], "_blank");
-			}
+			await convertFileFormat(fileType, `audio/${fileType}`, youtubeID);
 		}
 	} catch (e) {
 		console.error(e);
