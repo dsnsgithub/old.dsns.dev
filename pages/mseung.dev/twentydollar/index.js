d8f24ed4338b429b52839378b414013dcac38f6d
maxas10 <maxaseung@gmail.com>
2023-04-21 19:36:53 -0700

    twenty dollar website for mseung

---
+++ pages/mseung.dev/twentydollar/index.js
@@ -0,0 +1,166 @@
+const box = document.getElementById("box");
+const field = document.getElementById("setnumber");
+const tempofield = document.getElementById("tempofield");
+
+let speed = 200;
+/*--------*/
+
+/*intervals and timeouts */
+var wait;
+/*-------*/
+
+/*other variables */
+var blocklist = [];
+var x = -1;
+/*-------*/
+
+/* add stuff */
+function createNote() {
+    var newNote = document.createElement("div");
+    newNote.classList.add("block");
+    newNote.classList.add("note");
+    addtoBox(newNote);
+}
+
+function createDrum() {
+    var newDrum = document.createElement("div");
+    newDrum.classList.add("block");
+    newDrum.classList.add("drum");
+    addtoBox(newDrum);
+}
+
+function createConnect() {
+    var newConnect = document.createElement("div");
+    newConnect.classList.add("block");
+    newConnect.classList.add("connect");
+    addtoBox(newConnect);
+}
+
+function createTempo() {
+    var newTempo = document.createElement("div");
+    newTempo.classList.add("block");
+    newTempo.classList.add("tempo");
+    addtoBox(newTempo);
+}
+
+function createPause() {
+    var newPause = document.createElement("div");
+    newPause.classList.add("block");
+    newPause.classList.add("pause");
+    addtoBox(newPause);
+}
+
+function addtoBox(item) {
+    switch(item.className) {
+        case "block connect":
+            item.innerHTML = "=";
+            break;
+        case "block pause":
+            for(let i=0; i<Number(field.value) -1 ; i++) {
+                var newPause = document.createElement("div");
+                newPause.classList.add("block");
+                newPause.classList.add("pause");
+                blocklist.push(newPause);
+            }
+        default:
+            item.innerHTML = Number(field.value);
+            break;
+    }
+
+    blocklist.push(item)
+
+    box.appendChild(item);
+}
+/*------------*/
+
+/* others */
+function remove() {
+    if(blocklist.length > 0) {
+        blocklist[blocklist.length - 1].remove();
+        blocklist.pop();
+    } 
+}
+
+function play() {
+    wait = setInterval(read, speed)
+
+    function read() {
+        x++;
+        if(blocklist[x] !== undefined) {
+            for(let i=0; i<blocklist.length; i++) {
+                blocklist[i].style.border = "none";
+            }
+    
+            blocklist[x].style.border = "2px solid gold";
+    
+            switch(blocklist[x].className) {
+                case "block note":
+                    playAudio("middlec.mp3", 0.75);
+                    break;
+                case "block drum":
+                    playAudio("bassdrum.mp3", 2.25);
+                    break;
+                case "block connect":
+                    read();
+                    read();
+                    break;
+                case "block pause":
+                    break;
+                case "block tempo":
+                    speed = parseInt(blocklist[x].innerHTML);
+                    read();
+                    clearInterval(wait);
+                    wait = setInterval(read, speed);
+                    break;
+            }
+        } else {
+            stopInterval();
+        }
+    }
+
+    document.getElementById("play").innerHTML = "Stop";
+    document.getElementById("play").onclick = stopInterval;
+}
+
+function playAudio(source, time) {
+    var newSound = new Audio();
+    var soundsrc = document.createElement("source");
+    soundsrc.type = "audio/mpeg";
+    soundsrc.src = source;
+    newSound.appendChild(soundsrc);
+    newSound.currentTime = time;
+    if(Number(blocklist[x].innerHTML) + 1 > 15) {
+        alert("Maximum pitch is 14.");
+    } else {
+        newSound.playbackRate = Number(blocklist[x].innerHTML) + 1;
+    }
+    newSound.preservesPitch = false;
+    if(soundsrc.getAttribute("src") === "bassdrum.mp3") {
+        setTimeout(function() {
+            newSound.currentTime = 0;
+            newSound.pause();
+        }, 500)
+    }
+    newSound.play();
+}
+
+function stopInterval() {
+    clearInterval(wait);
+    x = -1;
+    speed = 200;
+
+    for(let i=0; i<blocklist.length; i++) {
+        blocklist[i].style.border = "none";
+    }
+
+    document.getElementById("play").innerHTML = "Play";
+    document.getElementById("play").onclick = play;
+}
+
+function clearAll() {
+    for(let i=0; i<blocklist.length; i++) {
+        blocklist[i].remove();
+    }
+    blocklist = [];
+}
+/*------------*/
