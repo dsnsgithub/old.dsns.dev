a8125c9c159269b3c63d01fa4aa71a0801cbde53
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-05-21 15:38:59 -0700

    add webosu.online

--- routes/proxy.js
+++ routes/proxy.js
@@ -3,15 +3,13 @@ const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middl
 // ! Line 77 of http-proxy-middleware.js in http-proxy-middleware must be changed to:
 // ! return contextMatcher.match(context, path, req) && this.proxyOptions.pathFilter(path, req);
 
-const pathFilter = function (path, req) {
-	return req.headers.host.startsWith("hide.dsns");
-};
-
 module.exports = function (app) {
 	app.use(
 		createProxyMiddleware({
 			target: "https://tetr.io/",
-			pathFilter: pathFilter,
+			pathFilter: function (path, req) {
+				return req.headers.host.startsWith("hide.dsns");
+			},
 			changeOrigin: true,
 			selfHandleResponse: true,
 			followRedirects: true,
@@ -21,6 +19,28 @@ module.exports = function (app) {
 				if (imageTypes.includes(proxyRes.headers["content-type"])) {
 					const response = responseBuffer.toString("utf8"); // convert buffer to string
 					return response.replace(/tetr.io/g, "hide.dsns.dev"); // manipulate response and return the result
+				}
+
+				return responseBuffer;
+			})
+		})
+	);
+
+	app.use(
+		createProxyMiddleware({
+			target: "https://webosu.online/",
+			pathFilter: function (path, req) {
+				return req.headers.host.startsWith("oss.dsns");
+			},
+			changeOrigin: true,
+			selfHandleResponse: true,
+			followRedirects: true,
+			ws: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript"];
+				if (imageTypes.includes(proxyRes.headers["content-type"])) {
+					const response = responseBuffer.toString("utf8"); // convert buffer to string
+					return response.replace(/webosu.online/g, "oss.dsns.dev"); // manipulate response and return the result
 				}
 
 				return responseBuffer;
