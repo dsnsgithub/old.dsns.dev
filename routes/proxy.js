abafe608db79a82eecf4fb86cfefca286eed2bd0
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-05-22 19:57:32 -0700

    remove osu proxy

--- routes/proxy.js
+++ routes/proxy.js
@@ -4,128 +4,25 @@ const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middl
 // ! return contextMatcher.match(context, path, req) && this.proxyOptions.pathFilter(path, req);
 
 module.exports = function (app) {
-	app.use(
-		createProxyMiddleware({
-			target: "https://tetr.io/",
-			pathFilter: function (path, req) {
-				return req.headers.host.includes("tetr");
-			},
-			changeOrigin: true,
-			selfHandleResponse: true,
-			followRedirects: true,
-			ws: true,
-			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/javascript"];
-				if (imageTypes.includes(proxyRes.headers["content-type"])) {
-					const response = responseBuffer.toString("utf8"); // convert buffer to string
-					return response.replace(/tetr.io/g, req.headers.host); // manipulate response and return the result
-				}
+	const options = {
+		target: "https://tetr.io/",
+		pathFilter: function (path, req) {
+			return req.headers.host.includes("tetr");
+		},
+		changeOrigin: true,
+		selfHandleResponse: true,
+		followRedirects: true,
+		ws: true,
+		onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+			const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/javascript"];
+			if (imageTypes.includes(proxyRes.headers["content-type"])) {
+				const response = responseBuffer.toString("utf8");
+				return response.replace(/tetr.io/g, req.headers.host);
+			}
 
-				return responseBuffer;
-			})
+			return responseBuffer;
 		})
-	);
+	};
 
-	app.use(
-		createProxyMiddleware({
-			target: "https://webosu.online/",
-			pathFilter: function (path, req) {
-				return req.headers.host.startsWith("oss.dsns");
-			},
-			changeOrigin: true,
-			selfHandleResponse: true,
-			followRedirects: true,
-			ws: true,
-			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
-				if (imageTypes.includes(proxyRes.headers["content-type"])) {
-					const response = responseBuffer.toString("utf8"); // convert buffer to string
-
-					const splitDomain = req.headers.host.split(".");
-					const domain = splitDomain[1] + "." + splitDomain[2];
-					return response
-						.replace(/webosu.online/g, req.headers.host)
-						.replace(/catboy.best/g, `catboybest.${domain}`)
-						.replace(/assets.ppy.sh/g, `assetsppysh.${domain}`)
-						.replace(/livescores.webosu.workers.dev/g, `livescores.${domain}`);
-				}
-
-				return responseBuffer;
-			})
-		})
-	);
-
-	app.use(
-		createProxyMiddleware({
-			target: "https://catboy.best/",
-			pathFilter: function (path, req) {
-				return req.headers.host.startsWith("catboybest.dsns");
-			},
-			changeOrigin: true,
-			selfHandleResponse: true,
-			followRedirects: true,
-			ws: true,
-			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-
-				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
-				if (imageTypes.includes(proxyRes.headers["content-type"])) {
-					const response = responseBuffer.toString("utf8"); // convert buffer to string
-
-					const splitDomain = req.headers.host.split(".");
-					const domain = splitDomain[1] + "." + splitDomain[2];
-					return response
-						.replace(/catboy.best/g, `catboybest.${domain}`)
-						.replace(/assets.ppy.sh/g, `assetsppysh.${domain}`)
-						.replace(/livescores.webosu.workers.dev/g, `livescores.${domain}`);
-				}
-
-				return responseBuffer;
-			})
-		})
-	);
-
-	app.use(
-		createProxyMiddleware({
-			target: "https://livescores.webosu.workers.dev/",
-			pathFilter: function (path, req) {
-				return req.headers.host.startsWith("livescores.dsns");
-			},
-			changeOrigin: true,
-			selfHandleResponse: true,
-			followRedirects: true,
-			ws: true,
-			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
-				if (imageTypes.includes(proxyRes.headers["content-type"])) {
-					const response = responseBuffer.toString("utf8"); // convert buffer to string
-
-					const splitDomain = req.headers.host.split(".");
-					const domain = splitDomain[1] + "." + splitDomain[2];
-					return response
-						.replace(/catboy.best/g, `catboybest.${domain}`)
-						.replace(/assets.ppy.sh/g, `assetsppysh.${domain}`)
-						.replace(/livescores.webosu.workers.dev/g, `livescores.${domain}`);
-				}
-
-				return responseBuffer;
-			})
-		})
-	);
-
-	app.use(
-		createProxyMiddleware({
-			target: "https://assets.ppy.sh/",
-			pathFilter: function (path, req) {
-				return req.headers.host.startsWith("assetsppysh.dsns");
-			},
-			changeOrigin: true,
-			selfHandleResponse: true,
-			followRedirects: true,
-			ws: true,
-			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				return responseBuffer;
-			})
-		})
-	);
+	app.use(createProxyMiddleware(options));
 };
