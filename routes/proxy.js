ee3c1272e367f141e7d68c01e45aeedb4ad23451
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-05-22 18:02:48 -0700

    fix osu proxy

--- routes/proxy.js
+++ routes/proxy.js
@@ -37,10 +37,89 @@ module.exports = function (app) {
 			followRedirects: true,
 			ws: true,
 			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript"];
+				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
 				if (imageTypes.includes(proxyRes.headers["content-type"])) {
 					const response = responseBuffer.toString("utf8"); // convert buffer to string
-					return response.replace(/webosu.online/g, "oss.dsns.dev"); // manipulate response and return the result
+					return response
+						.replace(/webosu.online/g, req.headers.host)
+						.replace(/catboy.best/g, "catboybest.dsns.dev")
+						.replace(/assets.ppy.sh/g, "assetsppysh.dsns.dev")
+						.replace(/livescores.webosu.workers.dev/g, "livescores.dsns.dev")
+				}
+
+				return responseBuffer;
+			})
+		})
+	);
+
+	app.use(
+		createProxyMiddleware({
+			target: "https://catboy.best/",
+			pathFilter: function (path, req) {
+				return req.headers.host.startsWith("catboybest.dsns");
+			},
+			changeOrigin: true,
+			selfHandleResponse: true,
+			followRedirects: true,
+			ws: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
+				if (imageTypes.includes(proxyRes.headers["content-type"])) {
+					const response = responseBuffer.toString("utf8"); // convert buffer to string
+					return response
+						.replace(/catboy.best/g, "catboybest.dsns.dev")
+						.replace(/assets.ppy.sh/g, "assetsppysh.dsns.dev")
+						.replace(/livescores.webosu.workers.dev/g, "livescores.dsns.dev")
+				}
+
+				return responseBuffer;
+			})
+		})
+	);
+
+	app.use(
+		createProxyMiddleware({
+			target: "https://livescores.webosu.workers.dev/",
+			pathFilter: function (path, req) {
+				return req.headers.host.startsWith("livescores.dsns");
+			},
+			changeOrigin: true,
+			selfHandleResponse: true,
+			followRedirects: true,
+			ws: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
+				if (imageTypes.includes(proxyRes.headers["content-type"])) {
+					const response = responseBuffer.toString("utf8"); // convert buffer to string
+					return response
+						.replace(/catboy.best/g, "catboybest.dsns.dev")
+						.replace(/assets.ppy.sh/g, "assetsppysh.dsns.dev")
+						.replace(/livescores.webosu.workers.dev/g, "livescores.dsns.dev")
+				}
+
+				return responseBuffer;
+			})
+		})
+	);
+
+	app.use(
+		createProxyMiddleware({
+			target: "https://assets.ppy.sh/",
+			pathFilter: function (path, req) {
+				return req.headers.host.startsWith("assetsppysh.dsns");
+			},
+			changeOrigin: true,
+			selfHandleResponse: true,
+			followRedirects: true,
+			ws: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				const imageTypes = ["text/css", "text/html; charset=UTF-8", "text/html; charset=utf-8", "text/javascript", "application/json", "application/javascript; charset=utf-8"];
+				if (imageTypes.includes(proxyRes.headers["content-type"])) {
+					const response = responseBuffer.toString("utf8"); // convert buffer to string
+					return response
+						.replace(/catboy.best/g, "catboybest.dsns.dev")
+						.replace(/assets.ppy.sh/g, "assetsppysh.dsns.dev")
+						.replace(/livescores.webosu.workers.dev/g, "livescores.dsns.dev")
 				}
 
 				return responseBuffer;
