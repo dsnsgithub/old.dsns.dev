d87ff953dd708989adbf5b01378c804d188b64d9
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2023-06-27 15:27:00 -0700

    add dynmap to proxy

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,25 +1,28 @@
 const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-	const options = {
-		target: "https://tetr.io/",
-		changeOrigin: true,
-		selfHandleResponse: true,
-		followRedirects: true,
-		ws: true,
-		onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-			const contentHeader = proxyRes.headers?.["content-type"] || proxyRes.headers?.["Content-Type"];
-			if (!contentHeader) return responseBuffer;
+	const createOptions = (target, domainReplacement) => {
+		return {
+			target: target,
+			changeOrigin: true,
+			selfHandleResponse: true,
+			followRedirects: true,
+			ws: true,
+			onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+				const contentHeader = proxyRes.headers?.["content-type"] || proxyRes.headers?.["Content-Type"];
+				if (!contentHeader) return responseBuffer;
 
-			const contentType = contentHeader.toLowerCase();
-			if (contentType.includes("text/") || contentType.includes("utf-8")) {
-				const response = responseBuffer.toString("utf8");
-				return response.replace(/tetr.io/g, req.headers.host);
-			}
+				const contentType = contentHeader.toLowerCase();
+				if (contentType.includes("text/") || contentType.includes("utf-8")) {
+					const response = responseBuffer.toString("utf8");
+					return response.replace(new RegExp(domainReplacement, "g"), req.headers.host);
+				}
 
-			return responseBuffer;
-		})
+				return responseBuffer;
+			})
+		};
 	};
 
-	app.use(createProxyMiddleware((_, req) => req.headers.host.includes("tetr"), options));
+	app.use(createProxyMiddleware((_, req) => req.headers.host.includes("tetr"), createOptions("https://tetr.io/", "tetr.io")));
+	app.use(createProxyMiddleware((_, req) => req.headers.host.includes("map"), createOptions("http://10.3.3.191:8123", "10:3.3.191:8123")));
 };
